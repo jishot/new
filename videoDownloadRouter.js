2d3d1188f6ab664b56b3fb78ca6518bbf899c61d
jishot <myjgreen1750@gmail.com>
2024-09-17 16:02:23 -0400

    Update videoDownloadRouter.js

--- videoDownloadRouter.js
+++ videoDownloadRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const puppeteer = require('puppeteer');
 const archiver = require('archiver');
 const fs = require('fs');
-const fetch = require('node-fetch');
+const fetch = require('node-fetch').default;
 const ffmpeg = require('fluent-ffmpeg');
 
 const router = express.Router();

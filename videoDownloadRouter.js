e5f67f35f31d45fbe36ac9b1c4c930d610773a37
jishot <myjgreen1750@gmail.com>
2024-09-17 14:28:35 -0400

    Update videoDownloadRouter.js

--- videoDownloadRouter.js
+++ videoDownloadRouter.js
@@ -2,38 +2,40 @@ const express = require('express');
 const puppeteer = require('puppeteer');
 const archiver = require('archiver');
 const axios = require('axios');
+const { URL } = require('url'); // Import the URL module
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const { url } = req.query;
+    const { url } = req.query;
 
-  if (!url) {
-    return res.status(400).send('Missing URL parameter');
-  }
+    if (!url) {
+        return res.status(400).send('Missing URL parameter');
+    }
 
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
 
-  await page.goto(url);
+    await page.goto(url);
 
-  const videoUrls = await page.evaluate(() => {
-    const videoElements = document.querySelectorAll('.frame-block.thumb-block');
-    return Array.from(videoElements).map((videoElement) => videoElement.querySelector('a').getAttribute('href'));
-  });
+    const videoUrls = await page.evaluate(() => {
+        const videoElements = document.querySelectorAll('.frame-block.thumb-block');
+        return Array.from(videoElements).map((videoElement) => videoElement.querySelector('a').getAttribute('href'));
+    });
 
-  const archive = archiver('zip', { zlib: { level: 9 } });
-  res.attachment('videos.zip');
-  archive.pipe(res);
+    const archive = archiver('zip', { zlib: { level: 9 } });
+    res.attachment('videos.zip');
+    archive.pipe(res);
 
-  for (const videoUrl of videoUrls) {
-    const videoResponse = await axios.get(videoUrl, { responseType: 'stream' });
-    archive.append(videoResponse.data, { name: `video_${videoUrl.split('/').pop()}.mp4` });
-  }
+    for (const videoUrl of videoUrls) {
+        const absoluteUrl = new URL(videoUrl, url).href; // Convert relative URL to absolute URL
+        const videoResponse = await axios.get(absoluteUrl, { responseType: 'stream' });
+        archive.append(videoResponse.data, { name: `video_${absoluteUrl.split('/').pop()}.mp4` });
+    }
 
-  archive.finalize();
+    archive.finalize();
 
-  await browser.close();
+    await browser.close();
 });
 
 module.exports = router;
